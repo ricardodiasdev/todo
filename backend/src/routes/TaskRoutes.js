2394b479bead315a334e5d03fd13e05f8966a73b
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-21 20:10:15 -0300

    Listando todas as tarefas e middleware do macaddress

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const TaskController = require("../controllers/TaskController");
-const TaskValidation = require("../middlewares/TaskValidation")
+const TaskValidation = require("../middlewares/TaskValidation");
+const MacaddressValidation = require("../middlewares/MacaddressValidation");
 
 router.post("/", TaskValidation, TaskController.create);
 router.put("/:id", TaskValidation, TaskController.update);
+router.get("/filter/all", MacaddressValidation, TaskController.all);
 
 module.exports = router;
