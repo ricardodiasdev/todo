1407a7aad7327df94288fd9eef5f61245b7a7c38
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-21 16:18:32 -0300

    Middleware de validação de tarefas

---
+++ backend/src/middlewares/TaskValidation.js
@@ -0,0 +1,39 @@
+const TaskModel = require("../model/TaskModel");
+const { isPast } = require("date-fns");
+const TaskValidation = async (req, res, next) => {
+  const { macaddress, type, title, description, when } = req.body;
+
+  if (!macaddress) {
+    return res.status(400).json({ error: "Mac Address é obrigatório..." });
+  } else if (!type) {
+    return res
+      .status(400)
+      .json({ error: "Típo da atividade é obrigatório..." });
+  } else if (!title) {
+    return res
+      .status(400)
+      .json({ error: "Título da atividade é obrigatório..." });
+  } else if (!description) {
+    return res.status(400).json({ error: "Descrição é obrigatória..." });
+  } else if (!when) {
+    return res.status(400).json({ error: "Data e Hora são obrigatórios..." });
+  } else if (isPast(new Date(when))) {
+    return res.status(400).json({ error: "Escolha uma hora futura..." });
+  } else {
+   
+    let exists;
+    exists = await TaskModel.findOne({
+      'when': { '$eq': new Date(when) },
+      'macaddress': { $in: req.body.macaddress },
+    });
+    if (exists) {
+      return res
+        .status(400)
+        .json({ error: "Já existe atividade nessa data e hora..." });
+    }
+   
+    next();
+  }
+};
+
+module.exports = TaskValidation;
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const TaskController = require("../controllers/TaskController");
+const TaskValidation = require("../middlewares/TaskValidation")
 
-router.post("/task", TaskController.create);
+router.post("/task", TaskValidation, TaskController.create);
 
 module.exports = router;
