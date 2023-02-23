94bd650a716e700f715fa15e8c248c4e35c3cf2f
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-23 10:25:06 -0300

    Criando o Footer

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./styles";
+
+
+
+function Footer() {
+  return (
+    <S.Container>
+      <span>ToDo - Organizador de tarefas</span>
+    </S.Container>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/components/Footer/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 50px;
+  background: #20295f;
+  border-top: 5px solid #ee6b26;
+  display: flex;
+
+  position: fixed;
+  bottom: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    color: #fff;
+  }
+`;
