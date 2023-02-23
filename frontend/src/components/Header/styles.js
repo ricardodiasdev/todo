94cf9c6e6e734a87e51484a2d201ac06ed4a442f
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-23 09:48:54 -0300

    Criando o Footer com Styled Components

---
+++ frontend/src/components/Header/styles.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  background: #20295f;
+  border-bottom: 5px solid #ee6b26;
+  display: flex;
+
+  img {
+    width: 100px;
+    height: 50px;
+  }
+`;
+
+export const LeftSide = styled.div`
+  width: 50%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`;
+
+export const RightSide = styled.div`
+  width: 50%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-right: 10px;
+
+  a {
+    color: #fff;
+    font-weight: bold;
+    text-decoration: none;
+    margin: 0 10px;
+
+    &:hover {
+      color: #ee6b26;
+    }
+  }
+
+  #notification{
+    img{
+        width: 25px;
+        height: 30px
+    }
+
+    span{
+        background: #FFF;
+        color: #EE6B26;
+        padding: 3px 7px;
+        border-radius: 50%;
+        position: relative;
+        top: -20px;
+        right: 10px;
+    }
+
+    &:hover{
+        opacity: 0.5;
+    }
+  }
+
+  .divider{
+    margin: 0 10px;
+    color: #fff;
+  }
+`;
--- frontend/src/views/Home/index.js
+++ frontend/src/views/Home/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import * as S from "./styles";
+
+import Header from "../../components/Header";
 
 function Home() {
-  return <h1>HOME</h1>
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  );
 }
 
 export default Home;
