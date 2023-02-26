427c41ee547bb206f0a21d162195233bd6740523
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-26 18:24:36 -0300

    Ocultando o sino quando não tem notificações

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -6,23 +6,27 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import bell from "../../assets/bell.png";
 
-function Header({lateCount, clickBell}) {
+function Header({ lateCount, clickBell }) {
   return (
     <S.Container>
       <S.LeftSide>
         <img src={logo} alt="Logomarca" />
       </S.LeftSide>
       <S.RightSide>
-        <Link to={'/'}>INÍCIO</Link>
+        <Link to={"/"}>INÍCIO</Link>
         <span className="divider">|</span>
-        <Link to={'/task'}>NOVA TAREFA</Link>
+        <Link to={"/task"}>NOVA TAREFA</Link>
         <span className="divider">|</span>
-        <Link to={'/qrcode'}>SINCRONIZAR TAREFA</Link>
-        <span className="divider">|</span>
-        <button id="notification" onClick={clickBell}>
-          <img src={bell} alt="Notificação" />
-          <span>{lateCount}</span>
-        </button>
+        <Link to={"/qrcode"}>SINCRONIZAR TAREFA</Link>
+        {lateCount && (
+          <>
+            <span className="divider">|</span>
+            <button id="notification" onClick={clickBell}>
+              <img src={bell} alt="Notificação" />
+              <span>{lateCount}</span>
+            </button>
+          </>
+        )}
       </S.RightSide>
     </S.Container>
   );
