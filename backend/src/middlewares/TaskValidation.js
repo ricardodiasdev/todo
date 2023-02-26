99a4274d62e0cc803ebb131aee6637482999b29e
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-26 10:01:29 -0300

    Refatorando API - status de tarefas passadas

--- backend/src/middlewares/TaskValidation.js
+++ backend/src/middlewares/TaskValidation.js
@@ -1,7 +1,7 @@
 const TaskModel = require("../model/TaskModel");
 const { isPast } = require("date-fns");
 const TaskValidation = async (req, res, next) => {
-  const { macaddress, type, title, description, when } = req.body;
+  const { macaddress, type, title, description, when, done } = req.body;
 
   if (!macaddress) {
     return res.status(400).json({ error: "Mac Address é obrigatório..." });
@@ -17,7 +17,7 @@ const TaskValidation = async (req, res, next) => {
     return res.status(400).json({ error: "Descrição é obrigatória..." });
   } else if (!when) {
     return res.status(400).json({ error: "Data e Hora são obrigatórios..." });
-  } else if (isPast(new Date(when))) {
+  } else if (isPast(new Date(when)) && !done) {
     return res.status(400).json({ error: "Escolha uma hora futura..." });
   } else {
     let exists;
