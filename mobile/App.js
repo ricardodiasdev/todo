3b2422da1b2f28a6b27e6c74423ca8b5120fcd67
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-27 17:49:24 -0300

    Tela home, task e estrutura de pastas e arquivos

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Home from "./src/views/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Começando o Mobile!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
