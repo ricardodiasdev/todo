a99dadbfd0f238deb8f1f758753921ebb8a55bd7
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-27 23:39:44 -0300

    Criação do componente header de maneira dinâmica

--- mobile/src/views/Home/index.js
+++ mobile/src/views/Home/index.js
@@ -3,9 +3,15 @@ import { View, Text } from "react-native";
 
 import styles from "./styles";
 
+import Header from "../../components/Header";
+
+//Componentes
+
+
 export default function Home() {
   return (
     <View style={styles.container}>
+      <Header showNotification={true} showBack={false}/>
       <Text>Bem vindo a tela Home</Text>
     </View>
   );
