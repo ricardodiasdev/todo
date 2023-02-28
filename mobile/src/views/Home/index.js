b6cc1da61c44453496af68af19d9bef24877ca0d
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-28 11:38:32 -0300

    Criação dos filtros

--- mobile/src/views/Home/index.js
+++ mobile/src/views/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 
 import styles from "./styles";
@@ -6,13 +6,71 @@ import styles from "./styles";
 //Componentes
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { TouchableOpacity } from "react-native";
 
 export default function Home() {
+  const [filter, setFilter] = useState("today");
   return (
     <View style={styles.container}>
       <Header showNotification={true} showBack={false} />
+      <View style={styles.filter}>
+        <TouchableOpacity onPress={() => setFilter("all")}>
+          <Text
+            style={
+              filter === "all"
+                ? styles.filterTextActived
+                : styles.filterTextInactived
+            }
+          >
+            Todos
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setFilter("today")}>
+          <Text
+            style={
+              filter === "today"
+                ? styles.filterTextActived
+                : styles.filterTextInactived
+            }
+          >
+            Hoje
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setFilter("week")}>
+          <Text
+            style={
+              filter === "week"
+                ? styles.filterTextActived
+                : styles.filterTextInactived
+            }
+          >
+            Semana
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setFilter("month")}>
+          <Text
+            style={
+              filter === "month"
+                ? styles.filterTextActived
+                : styles.filterTextInactived
+            }
+          >
+            Mês
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setFilter("year")}>
+          <Text
+            style={
+              filter === "year"
+                ? styles.filterTextActived
+                : styles.filterTextInactived
+            }
+          >
+            Ano
+          </Text>
+        </TouchableOpacity>
+      </View>
       <Footer icon={"add"} />
-      <Text>Bem vindo a tela Home</Text>
     </View>
   );
 }
--- mobile/src/views/Home/styles.js
+++ mobile/src/views/Home/styles.js
@@ -7,6 +7,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
   },
+  filter: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+    height: 70,
+    alignItems: "center",
+  },
+  filterTextActived: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#EE6B26",
+  },
+  filterTextInactived: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#20295f",
+    opacity: 0.5,
+  },
 });
 
 export default styles;
